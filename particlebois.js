635ca468a51c0762871870a482480902e92a7496
Jodie Wei <jodiewei2019@u.northwestern.edu>
2019-01-31 04:03:32 -0600

    user input count and speed

--- particlebois.js
+++ particlebois.js
@@ -2,6 +2,40 @@
 // BASIC SETUP
 // ------------------------------------------------
 
+
+//form stuff
+let count = 1;
+let speed = 1;
+
+
+onCountClick = (event) => {
+	count = document.getElementById("count").value;
+	console.log(count);
+	while(scene.children.length > 0){ 
+	    scene.remove(scene.children[0]); 
+	}
+	for(i = 0; i < count; i++) {
+		bois[i] = new createBoi();
+		bois[i].placeBoi();
+	}
+	render();
+}
+onSpeedClick = (event) => {
+	speed = document.getElementById("speed").value;
+	console.log(speed);
+}
+
+
+var countButton = document.getElementById("goCount");
+countButton.addEventListener("click", onCountClick, false);
+
+var speedButton = document.getElementById("goSpeed");
+speedButton.addEventListener("click", onSpeedClick, false);
+
+
+
+
+
 // Create an empty scene
 var scene = new THREE.Scene();
 
@@ -15,34 +49,37 @@ var renderer = new THREE.WebGLRenderer({antialias:true});
 // Configure renderer
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setClearColor( 0x002642, 1 );
+renderer.setClearColor( 0x002642, 1.5 );
 
 // Configure renderer size
 renderer.setSize( window.innerWidth, window.innerHeight );
 
 // Append Renderer to DOM
 document.body.appendChild( renderer.domElement );
+renderer.render (scene, camera);
 
-var ambientLight  = new THREE.AmbientLight( '#FF9BDA' ),
-	hemiLight     = new THREE.HemisphereLight('#FF9BDA', '#FF9BDA', 1000 ),
-	light         = new THREE.PointLight( '#FF9BDA', 1, 1000 );
-ambientLight.position.set( 0, 0, 0 );
-hemiLight.position.set( 0, 0, 0 );
-light.position.set( 0, 0, 10 );
-scene.add( ambientLight ); 
-scene.add( hemiLight );
-scene.add( light );
+
 
 //FUN STUFF
-createBoi = () => {
-	var geometry = new THREE.SphereGeometry(10,10,10);
-	var material = new THREE.MeshBasicMaterial({ color: 0xFFF3EF, wireframe:true} );
+var group = new THREE.Group();
+scene.add(group);
+
+var bois = [];
+
+function createBoi() {
+	var geometry = new THREE.SphereGeometry(0.3,10,10);
+	//var material = new THREE.MeshBasicMaterial({ color: 0xFFF3EF, wireframe:true} );
+	var material = new THREE.MeshBasicMaterial({ color: 'yellow'} );
 	var sphere = new THREE.Mesh(geometry, material);
-	var randomPos = [Math.random()*10, Math.random()*10, 0];
-	sphere.position.set(randomPos[0], randomPos[1], randomPos[2]);	
+	
 	scene.add(sphere);
 
-	moveBoi = () => {
+	this.placeBoi = () => {
+		var randomPos = [Math.random()*10, Math.random()*10, 0];
+		sphere.position.set(randomPos[0], randomPos[1], randomPos[2]);	
+	}
+
+	this.moveBoi = () => {
 		let x = sphere.position.x,
 			y = sphere.position.y;
 
@@ -54,7 +91,7 @@ createBoi = () => {
 		dX = xRange[Math.floor(Math.random() * xRange.length)]
 		dY = yRange[Math.floor(Math.random() * yRange.length)]
 
-		var magnitude = Math.sqrt(Math.abs(dX*dX)+Math.abs(dY*dY)) / 2.5;
+		var magnitude = Math.sqrt(Math.abs(dX*dX)+Math.abs(dY*dY)) / speed;
 
 
 		newX = x + dX / (magnitude);
@@ -63,6 +100,7 @@ createBoi = () => {
 		var frustum = new THREE.Frustum();
 		frustum.setFromMatrix(new THREE.Matrix4().multiplyMatrices(camera.projectionMatrix, camera.matrixWorldInverse)); 
 
+		//make sure they don't go offscreen
 		var pos = new THREE.Vector3(newX, newY, 0);
 		if (!frustum.containsPoint(pos)) {
 			console.log("beep")
@@ -74,18 +112,17 @@ createBoi = () => {
 
 	}
 
-	render = () => {
-		requestAnimationFrame( render );
-		moveBoi();
-		renderer.render (scene, camera);
-	}
-	render();
-
 }
 
-var myBoi = createBoi();
+render = () => {
 
+	requestAnimationFrame( render );
 
+	for(i = 0; i < bois.length; i++) {
+		bois[i].moveBoi();
+	}
+	renderer.render (scene, camera);
+}
 
 
 
